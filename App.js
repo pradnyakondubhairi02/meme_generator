107257929df48d8e3fcac71ca1028017c16a1efa
pradnya kondubhairi <91897968+pradnyakondubhairi02@users.noreply.github.com>
2023-01-25 22:08:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+// import Practice from './practice';
+import Header from './Header';
+import Meme from './Meme';
+
+
+function App() {
+  return (
+    
+    <>
+    
+        <Header/>
+        <Meme/>
+        {/* <Practice/> */}
+    </>
+  );
+}
+
+export default App;
